5c3a958cdecf15d38ae91e86fa8e1decf97d218a
Giovane Menezes <giovane.menezes@live.com>
2019-03-30 15:55:06 -0300

    Start project

---
+++ Source/Top HeadlineTests/Top_HeadlineTests.swift
@@ -0,0 +1,34 @@
+//
+//  Top_HeadlineTests.swift
+//  Top HeadlineTests
+//
+//  Created by Giovane Silva de Menezes Cavalcante on 30/03/19.
+//  Copyright © 2019 GSMenezes. All rights reserved.
+//
+
+import XCTest
+@testable import Top_Headline
+
+class Top_HeadlineTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
